72c276f0f86fe9713fe343bcdcb84a9f2451eab9
Caio <caiozs21@gmail.com>
2023-04-27 18:54:18 -0300

    add deploy mongodb

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port, console.log(`Server running in port :${port}`))
