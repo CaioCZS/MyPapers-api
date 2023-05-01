450830d0e072b127436f76c8a6581e056b9ffa6c
ana_btz <silva.anabtz@gmail.com>
2023-04-30 23:30:03 -0300

    feat: implement DELETE request

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -1,18 +1,18 @@
-import { db } from "../database/database.connection.js"
+import { ObjectId } from "mongodb";
+import { db } from "../database/database.connection.js";
 
 export async function getCartItens(req, res) {
-  const { userId } = res.locals.session
+  const { userId } = res.locals.session;
 
   try {
-    const cart = await db.collection("cart").find({ userId }).toArray()
-    console.log(cart)
+    const cart = await db.collection("cart").find({ userId }).toArray();
+    console.log(cart);
 
-    if (!cart) return res.status(404).send("Carrinho está vazio")
+    if (!cart) return res.status(404).send("Carrinho está vazio");
 
-    res.status(200).send(cart)
-
+    res.status(200).send(cart);
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
   }
 }
 
@@ -29,10 +29,18 @@ export async function updateCartIten(req, res) {
 
 export async function removeCartIten(req, res) {
   const { userId } = res.locals.session;
+  const { id } = req.params;
+
+  if (!id) return res.sendStatus(404);
 
   try {
-    const products = await db.collection("cart").find({ userId }).toArray();
-    res.send(products);
+    const result = await db
+      .collection("cart")
+      .deleteOne({ productId: new ObjectId(id), userId });
+
+    if (result.deletedCount === 0) return res.sendStatus(404);
+
+    res.status(202).send("Produto removido com sucesso");
   } catch (error) {
     res.status(500).send(error.message);
   }
