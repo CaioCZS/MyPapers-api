970da563767420958af19dd25d73b83fe908e600
Caio <caiozs21@gmail.com>
2023-05-02 15:53:09 -0300

    object id pro idUser /buy

--- src/controllers/itens.controllers.js
+++ src/controllers/itens.controllers.js
@@ -1,49 +1,45 @@
-import { ObjectId } from "mongodb";
-import { db } from "../database/database.connection.js";
+import { ObjectId } from "mongodb"
+import { db } from "../database/database.connection.js"
 
 export async function getItens(req, res) {
   try {
-    const itens = await db.collection("itens").find().toArray();
-    return res.send(itens);
+    const itens = await db.collection("itens").find().toArray()
+    return res.send(itens)
   } catch (err) {
-    res.status(500).send(err.message);
+    res.status(500).send(err.message)
   }
 }
 
 export async function getItemById(req, res) {
-  const { id } = req.params;
+  const { id } = req.params
 
   try {
-    const item = await db
-      .collection("itens")
-      .findOne({ _id: new ObjectId(id) });
-    res.send(item);
+    const item = await db.collection("itens").findOne({ _id: new ObjectId(id) })
+    res.send(item)
   } catch (err) {
-    res.status(500).send(err.message);
+    res.status(500).send(err.message)
   }
 }
 
 export async function buy(req, res) {
-  const { id } = req.params;
-  const { userId } = req.body;
+  const { id } = req.params
+  const { userId } = req.body
 
   try {
-    const item = await db
-      .collection("itens")
-      .findOne({ _id: new ObjectId(id) });
+    const item = await db.collection("itens").findOne({ _id: new ObjectId(id) })
 
     const buyItem = {
-      userId: userId,
+      userId: new ObjectId(userId),
       productId: item._id,
       name: item.name,
       image: item.image,
       price: item.price,
       description: item.description,
       quantity: 1,
-    };
-    await db.collection("cart").insertOne(buyItem);
-    return res.send(buyItem);
+    }
+    await db.collection("cart").insertOne(buyItem)
+    return res.send(buyItem)
   } catch (err) {
-    res.status(500).send(err.message);
+    res.status(500).send(err.message)
   }
 }
